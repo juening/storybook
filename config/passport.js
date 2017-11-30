e38b103d9db9e51af339ecf220e6c659a14af0a7
Jue Ning <juening@gmail.com>
2017-11-29 21:42:54 -0500

    added env keys

--- config/passport.js
+++ config/passport.js
@@ -6,8 +6,8 @@ module.exports = function(passport) {
   passport.use(
     new GoogleStrategy(
       {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
+        clientID: process.env.clientID || keys.googleClientID,
+        clientSecret: process.env.clientSecret || keys.googleClientSecret,
         callbackURL: '/auth/google/callback',
         proxy: true
       },
